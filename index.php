71a2a5e87249bae3c25e9fddcf26a9f096ff76bf
Fabio Dias <fabiowebmain@gmail.com>
2020-03-06 13:54:52 -0300

    Refatoração

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
+
+use Dinosaur\File;
+
 include_once 'class/Gravitational_formula.php';
 include_once 'class/File.php';
 
 define('METER_PER_SECOND', 9.8);
 
 $gravitationalFormula = new Gravitational_formula(METER_PER_SECOND);
-$file = new \Dinosaur\File();
+
+$file = new File();
 
 $f1 = $file->fileOpen("1");
 $f2 = $file->fileOpen("2");
 
-$dinosaursP = array_filter(array_merge_recursive($f1, $f2), function ($value) {
-    return $value["STANCE"] == "bipedal" && $value['LEG_LENGTH'];
-},ARRAY_FILTER_USE_BOTH);
-
+$dinosaursP = $file->filterAndMarge($f1, $f2);
 
 foreach ($dinosaursP as $key => $dp) {
     $dinosaursP[$key]['speed'] =
@@ -23,5 +24,6 @@ foreach ($dinosaursP as $key => $dp) {
 usort($dinosaursP, function ($a, $b) {
     return $a['speed'] - $b['speed'];
 });
+
 $file->writeFile($dinosaursP);
 
